999ec346807746ef1f0297e459453e8097c00d36
Benn <bennbey47@gmail.com>
2021-12-17 17:27:10 +0100

    Perso.html V3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import "./scss/main.scss";
+ const axios = require("axios")
+
+
+
+function getChaussures (){
+axios.get("http://localhost/WP_Baskin/wp-json/wp/v2/chaussures")
+ .then(function(reponse){
+     console.log(reponse)
+     let chaussures = reponse.data
+     chaussures.forEach(function(chaussure){
+         
+        console.log(chaussure)
+        console.log("L'id est : " , chaussure.id)
+        console.log("La date est :" , chaussure.date)
+        console.log("Le titre est :" , chaussure.title.rendered)
+
+        let mamarque = chaussure.maqrues
+        let couleurLacet = chaussure.acf.lacet
+
+        mamarque.forEach(function(marque){
+            console.log("L'ID marque est :" , marque)
+        })
+
+        couleurLacet.forEach(function(couleur){
+
+            console.log("la couleur est : " , couleur["lacet-couleur"] )
+        })
+
+     })     
+ }) 
+ .catch(function(error){
+     console.log("il y a une erreur !")
+     console.log(error)
+ })
+}
+
+getChaussures()
+
+ 
+/*id date marque titre*/ 
+
+/* note => (si .lacet-couleur ça ne marche pas ) => do => ["lacet-couleur"]*/
